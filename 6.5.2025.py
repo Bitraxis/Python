8a20a7daef11160e22d7c2474e1d8ff8cfb8a601
Bitraxis <hyauzane@protonmail.com>
2025-05-20 14:45:25 +0200

    Koniec

--- 6.5.2025.py
+++ 6.5.2025.py
@@ -1,6 +1,6 @@
 import pygame
 from sys import exit as sysexit
-from auticko import Auticko,Protivnik
+from auticko import Auticko, Protivnik
 
 pygame.init()
 
@@ -9,32 +9,43 @@ okno = pygame.display.set_mode(rozmery)
 FPS = 60
 Hra = True
 
-x, y, rychlost,zmena = 200,200,5,2
+x, y, rychlost, zmena = 200, 200, 5, 2
 auto = Auticko("auto.png", x, y, rychlost, rozmery)
 auta = pygame.sprite.Group()
+
 for i in range(5):
-    auta.add(Protivnik("auto.png", x*zmena, y*zmena, rychlost+zmena, rozmery))
+    auta.add(Protivnik("auto.png", x * zmena, y * zmena, rychlost + zmena, rozmery))
     zmena += 1
+
 while Hra:
     hodiny = pygame.time.Clock()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            Hra = False
             pygame.quit()
             sysexit()
     klavesy = pygame.key.get_pressed()
-    if klavesy[pygame.K_LEFT] :
+    if klavesy[pygame.K_LEFT]:
         auto.dolava()
-    if klavesy[pygame.K_RIGHT] :
+    if klavesy[pygame.K_RIGHT]:
         auto.doprava()
-    if klavesy[pygame.K_DOWN] :
+    if klavesy[pygame.K_DOWN]:
         auto.dole()
-    if klavesy[pygame.K_UP] :
+    if klavesy[pygame.K_UP]:
         auto.hore()
     auta.update()
     okno.fill((0, 0, 0))
-    okno.blit(auto.sprite,auto.pozicia)
+    okno.blit(auto.sprite, auto.pozicia)
     for auto2 in auta:
-        okno.blit(auto2.sprite,auto2.pozicia)
+        okno.blit(auto2.sprite, auto2.pozicia)
+    kolizia = pygame.sprite.spritecollide(auto, auta, False)
+    if len(kolizia) != 0:
+        print("zrazka")
+        Hra = False
+        pygame.quit()
+        sysexit()
+        
     pygame.display.update()
     hodiny.tick(FPS)
 pygame.quit()
--- auticko.py
+++ auticko.py
@@ -1,36 +1,48 @@
 import pygame
 from random import randrange
-class Auticko():
+
+
+class Auticko:
     def __init__(self, obrazok, x, y, pohyb, rozmery):
-        self.pozicia = [x,y]
+        self.pozicia = [x, y]
         self.pohyb = pohyb
         self.spriteOrig = pygame.image.load(obrazok)
         self.sprite = pygame.image.load(obrazok)
         self.rozmery = rozmery
-        
+        self.rect = self.sprite.get_rect(topleft=self.pozicia)
+
     def dolava(self):
         if self.pozicia[0] > 0:
             self.pozicia[0] -= self.pohyb
             self.sprite = pygame.transform.rotate(self.spriteOrig, 90)
+
     def doprava(self):
         if self.pozicia[0] < self.rozmery[0] - 100:
             self.pozicia[0] += self.pohyb
             self.sprite = pygame.transform.rotate(self.spriteOrig, -90)
+
     def hore(self):
         if self.pozicia[1] > 0:
             self.pozicia[1] -= self.pohyb
             self.sprite = pygame.transform.rotate(self.spriteOrig, 0)
+
     def dole(self):
         if self.pozicia[1] < self.rozmery[1] - 100:
             self.pozicia[1] += self.pohyb
             self.sprite = pygame.transform.rotate(self.spriteOrig, 180)
             
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=self.pozicia)
+
+
 class Protivnik(pygame.sprite.Sprite, Auticko):
     def __init__(self, obrazok, x, y, pohyb, rozmery):
         Auticko.__init__(self, obrazok, x, y, pohyb, rozmery)
         pygame.sprite.Sprite.__init__(self)
         self.smeruje = 0
+
     def update(self):
+        Auticko.update(self)
         if randrange(10) == randrange(10):
             self.smeruje = randrange(5)
         if self.smeruje == 1:
@@ -41,4 +53,3 @@ class Protivnik(pygame.sprite.Sprite, Auticko):
             self.dole()
         elif self.smeruje == 4:
             self.dolava()
-            
